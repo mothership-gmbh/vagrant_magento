8f7192a3e5c8b169054a2046e4ac177761fa00da
Maurizio Brioschi <maurizio.brioschi@ridesoft.org>
2015-11-13 10:06:33 +0100

    Update docker-restart.sh

--- puppet/modules/tools/templates/docker-restart.sh
+++ puppet/modules/tools/templates/docker-restart.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 docker rm $(docker ps -a -q)
 docker run -d --name=mariadb -p 127.0.0.1:3306:3306 -v /opt/mariadb/data:/data -e USER="super" -e PASS="super123" paintedfox/mariadb
-docker run -d -p 9200:9200 -p 9300:9300 -v /home/docker/elasticsearch:/data elasticsearch:1.7.3
-/usr/share/elasticsearch/bin/elasticsearch -Des.config=/data/elasticsearch.yml -Des.config=/data/elasticsearch.yml
+docker run -d -p 9200:9200 -p 9300:9300 -v /home/docker/elasticsearch:/data elasticsearch:1.7.3 /usr/share/elasticsearch/bin/elasticsearch -Des.config=/data/elasticsearch.yml -Des.config=/data/elasticsearch.yml
