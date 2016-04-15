555436e100fb076db6d2eaf68fa6bd91a5888c7d
Don Bosco van Hoi <donbosco.rulz@gmail.com>
2016-04-15 15:02:55 +0200

    Update docker-restart.sh

--- puppet/modules/tools/templates/docker-restart.sh
+++ puppet/modules/tools/templates/docker-restart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker rm $(docker ps -a -q)
 docker run -d --name=mariadb -p 127.0.0.1:3306:3306 -v /opt/mariadb/data:/data -e USER="super" -e PASS="super123" paintedfox/mariadb
-docker run -d -p 9200:9200 -p 9300:9300 -v /home/docker/elasticsearch:/data elasticsearch:1.7.3 /usr/share/elasticsearch/bin/elasticsearch -Des.config=/data/elasticsearch.yml -Des.config=/data/elasticsearch.yml
+docker run -d -p 9200:9200 -p 9300:9300 -v /home/docker/elasticsearch/data:/usr/share/elasticsearch/data -v /home/docker/elasticsearch/config/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml elasticsearch:2.1 /usr/share/elasticsearch/bin/elasticsearch -Des.insecure.allow.root=true
